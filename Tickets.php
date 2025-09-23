9e6de2913d812bfefcbb87c7c7c267a6e7c3e5a9
johnnysheps <johnsheppard281@gmail.com>
2025-09-23 12:16:02 -0400

    Tickets added

---
+++ Tickets.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="our ticket prices are $25.00 for Adult tickets, $20.00 forSenior (60+)/Student tickets, and $10.00 for a Child (12 and under) ticket; link to Zeffy to purchase a tickts online" />
+<meta name="keywords" content="The Pirates of Penzance, Rose Valley, Delaware Valley, theater, theatre, performance, orchestra, Rose Valley, Media, Springfield" />
+<title>Tickets - RVCO</title>
+<link rel="shortcut icon" href="images/rvco.ico" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">	
+<link href="css/multiColumnTemplate.css" rel="stylesheet" type="text/css">
+<link href="css/multiColumn_Max425.css" rel="stylesheet" type="text/css">
+<link href="css/multiColumn_Min426Max768.css" rel="stylesheet" type="text/css">
+<link href="css/multiColumn_Min769Max1000.css" rel="stylesheet" type="text/css">
+<link href="css/multiColumn_Min1001.css" rel="stylesheet" type="text/css">
+
+<!-- First uploaded Januart 16, 2024 -->
+<style>
+            button.zeffy-btn {
+                background-color: #YOURHEXCOLOUR;
+                border: none;
+                border-radius: 5px;
+                box-sizing: border-box;
+                color: white;
+                cursor: pointer;
+                left: calc(50% - 75px);
+                margin: 10px;
+                min-height: 50px;
+                min-width: 150px;
+                padding: 5px 10px;
+                text-transform: uppercase;
+                top: calc(50% - 25px);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>     
+
+<script type="text/javascript">
+
+function MM_showHideLayers() { //v9.0
+  var i,p,v,obj,args=MM_showHideLayers.arguments;
+  for (i=0; i<(args.length-2); i+=3) 
+  with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
+    if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+    obj.visibility=v; }
+}
+function MM_changeProp(objId,x,theProp,theValue) { //v9.0
+  var obj = null; with (document){ if (getElementById)
+  obj = getElementById(objId); }
+  if (obj){
+    if (theValue == true || theValue == false)
+      eval("obj.style."+theProp+"="+theValue);
+    else eval("obj.style."+theProp+"='"+theValue+"'");
+  }
+}
+
+</script>
+    
+</head>
+<body>
+<div class="container">     
+     <header>
+    <div class="primary_header">
+		 <?php include("header_new.php"); ?>
+    </div>
+    <nav><div class="secondary_header" id="menu">
+      <ul>
+            <li><a href="index.php">HOME</a></li>
+           <li><a href="AboutUs.php">ABOUT</a></li>
+             <li>TICKETS</li>
+		  <li><a href="Venue.php">VENUE</a></li>
+		  <li><a href="Auditions.php">AUDITIONS</a></li>
+            <li><a href="ContactUS.php"><nobr>CONTACT US</nobr></a></li>
+      </ul>
+    </div>
+    </nav>
+  </header>
+  <section>
+    <h2 class="noDisplay">Main Content</h2>
+                   <article class="left_article">
+                              <h1 id="pageName">TICKETS</h1>
+                              <h2 class="storyHead" style="visibility: hidden;"><i>State Fair</i></h2>
+                              <div style="margin: 20px;"><img style="background-color: white;  max-width: 800px;margin-left: auto;margin-right: auto;" src="gondoliers/logo/Gondoliers logo.webp" alt="The State Fair logo" class="img-responsive" name="statefair"  title="" hspace="0" vspace="0" /></div>
+
+                              <div id="tickets">
+                                   <p style="max-width: 800px;margin-left: auto;margin-right: auto;font-size: 1.1em;color:#003366;"><b>Are you looking for a gift idea? Tickets make a great present.</b></p>
+                                                  <p class="ticketsButton"><a class="ticketZeffy" title="Go to Zeffy to Buy Tickets to Glbert & Sullivan's The Gondoliers" href="https://www.zeffy.com/ticketing/the-gondoliers-tickets">Buy Tickets</a></p>
+                                                  <p>Peformances are at <a href="Venue.php">Strath Haven Middle School</a>, 200 South Providence Road, Wallingford, PA 19086.</p>
+
+                                                  <p>All tickets are general admission and good for <b><i>any</i></b> performance.
+                                                       <ul class="ticketPrice" style="margin-top: -10px;">Adult - $25.00</ul>
+                                                       <ul class="ticketPrice">Senior (60+)/Student - $20.00</ul>
+                                                       <ul class="ticketPrice">Child (12 and under) - $10.00</ul>
+                                                  </p>
+                                                  <p>For information on Group tickets, e-mail <a href="mailto:groups@rvco.org">groups@rvco.org</a>.
+                                                      <ul style="margin-left: 25px; margin-top: -10px;"><li>$15 each for 10 or more tickets</li>
+                                                      <li>Tickets must be for the same performance</li></ul></p>
+                                   <p>Performances:
+                                                       <ul class="showDates" style="margin-top: -10px;">Saturday, November 8 at 2 p.m. and 8 p.m.</ul>
+                                                       <ul class="showDates">Sunday, November 9 at 2 p.m.</ul>
+                                                       <ul class="showDates">Wednesday, November 12 at 8 p.m.</ul>
+                                                       <ul class="showDates">Saturday, November 15 at 2 p.m. and 8 p.m.</ul>
+                                                       <ul class="showDates">Sunday, November 16 at 2 p.m.</ul>
+                                                  <!--<ul><span style="font-size: 0.8em;">(Show dates and times are subject to final confirmation with the venue.)</span></ul>-->
+                                                  </p>
+                                                  
+                                                  <p>For questions, e-mail <a href="mailto:tickets@rvco.org">tickets@rvco.org</a> or call : <font class="high">610-565-5010</font><!--<font class="high">484-981-9108</font>--></p>
+                                   
+	  
+<!-- Inclement Weather Cancelation message	  
+	  <div style="border: solid #8E1B1D 2px;margin-bottom: 20px;text-align: center;max-width:800px"><span style="font: Arial; font-size: 24px;font-weight: bold">ATTENTION! No performance tonight!</span><br />
+		Due to the school canceling all evening activities because of inclement weather, tonight's performance (Thursday, November 15) has been cancelled.<br /><br /> 
+		We will see you on Saturday (2 pm and 8 pm performances) and Sunday (2 pm performance)!<br /><br /></div>
+End of Inclement Weather Cancelatio message -->	  
+
+                              </div>
+                    </article>
+  </section>
+     
+     
+     
+<footer class="secondary_header, footer">
+    <div class="copyright">
+ <?php include("footer.php"); ?></div>
+  </footer>
+</div>
+</body>
+</html>
