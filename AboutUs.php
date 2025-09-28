e505324e0c4aea9bfccf53dd1ee79cf41fc79de5
johnnysheps <johnsheppard281@gmail.com>
2025-09-28 19:30:17 -0400

    css update for home

--- AboutUs.php
+++ AboutUs.php
@@ -1,69 +1,6 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>About Us - RVCO</title>
-<link rel="shortcut icon" href="images/rvco.ico" />
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">	
-<link href="css/multiColumnTemplate.css" rel="stylesheet" type="text/css">
-<link href="css/multiColumn_Max425.css" rel="stylesheet" type="text/css">
-<link href="css/multiColumn_Min426Max768.css" rel="stylesheet" type="text/css">
-<link href="css/multiColumn_Min769Max1000.css" rel="stylesheet" type="text/css">
-<link href="css/multiColumn_Min1001.css" rel="stylesheet" type="text/css">
 
-<!-- First uploaded Januart 16, 2024 -->
-     
-     
+<h1 id="pageName">ABOUT US</h1>
 
-<script type="text/javascript">
-<!--
-function MM_showHideLayers() { //v9.0
-  var i,p,v,obj,args=MM_showHideLayers.arguments;
-  for (i=0; i<(args.length-2); i+=3) 
-  with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
-    if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
-    obj.visibility=v; }
-}
-function MM_changeProp(objId,x,theProp,theValue) { //v9.0
-  var obj = null; with (document){ if (getElementById)
-  obj = getElementById(objId); }
-  if (obj){
-    if (theValue == true || theValue == false)
-      eval("obj.style."+theProp+"="+theValue);
-    else eval("obj.style."+theProp+"='"+theValue+"'");
-  }
-}
-//-->
-</script>
-    
-</head>
-<body>
-<div class="container">
-  <header>
-    <div class="primary_header">
-		 <?php include("header_new.php"); ?>
-    </div>
-    <nav>
-         <div class="secondary_header" id="menu">
-      <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li>ABOUT</li>
-            <li><a href="Tickets.php">TICKETS</a></li>
-		  <li><a href="Venue.php">VENUE</a></li>
-		  <li><a href="Auditions.php">AUDITIONS</a></li>
-           <li><a href="Contacts.php"><nobr>CONTACT US</nobr></a></li>
-      </ul>
-     </div>
-    </nav>
- </header>
-
-     
-<div id="content">
-  <h1 id="pageName">ABOUT US</h1>
-    
-    
   <div id="aboutus"><img src="images/RVCO_small.gif" alt="" name="Picture8" id="rvcoLogo" />
     <p>The Rose Valley Chorus &amp; Orchestra presented its first production, Gilbert &amp; Sullivan's <i>The Mikado</i>, at the Artsman's Hall (now Hedgerow Theatre) in Rose Valley on November 21, 1907. This performance united the wealth of musical and dramatic talent residents in the arts and crafts colony of Rose Valley into a group that is still vibrant and successful for more than 100 years (and almost 200 shows! See a list of our <a href="productionHistory.php">production history</a>. Today we perform in the beautiful and modern theater at <a href="Venue.php">Strath Haven Middle School</a> in Wallingford.</p>
     
@@ -72,20 +9,3 @@ function MM_changeProp(objId,x,theProp,theValue) { //v9.0
     <p>In 1997 Rose Valley Chorus &amp; Orchestra purchased the old Middletown Firehouse in Media. It serves as our set construction site and houses our extensive costume and prop collection.</p>
     <p>In keeping with its long-standing traditions and policies, Rose Valley Chorus &amp; Orchestra considers those seeking to participate in its programming and productions on the basis of individual merit.  RVCO does not discriminate on the basis of race, color, religion, sex, sexual orientation, gender identity, national or ethnic origin, age, status as an individual with a disability, protected veteran status, genetic information, or other protected classes under the law.</p>
   </div>
-</div>
-<!--end content -->
-
-     
-<!--begin sponsor and donate -->          
- <?php include("social.php"); ?> 
- <?php include("join.php"); ?>     
-<!--end sponsor and donate -->     
-
-     
-<footer class="secondary_header, footer">
-    <div class="copyright">
- <?php include("footer.php"); ?></div>
-  </footer>
-</div>
-</body>
-</html>
