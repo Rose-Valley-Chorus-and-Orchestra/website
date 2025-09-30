ea715b26f78bebf23c72dce44182b4bb644e2304
johnnysheps <johnsheppard281@gmail.com>
2025-09-30 14:57:54 -0400

    about spacing

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
               </div>
 
               <div class="row mt-4">
-                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
                   <p>The Rose Valley Chorus &amp; Orchestra presented its first production, Gilbert &amp; Sullivan's <i>The Mikado</i>, at the Artsman's Hall (now Hedgerow Theatre) in Rose Valley on November 21, 1907. This performance united the wealth of musical and dramatic talent residents in the arts and crafts colony of Rose Valley into a group that is still vibrant and successful for more than 100 years (and almost 200 shows! See a list of our <a href="productionHistory.php">production history</a>. Today we perform in the beautiful and modern theater at <a href="Venue.php">Strath Haven Middle School</a> in Wallingford.</p>
                   
                   <p>The Rose Valley Chorus &amp; Orchestra draws its membership from the entire Delaware Valley, a result of its reputation for presenting professional-quality theatrical productions. The voices of the principals and chorus, with the musicianship of the full 35-piece orchestra, rank among the best of the area's many community musical theater groups.</p>
@@ -22,7 +22,7 @@
                   <p>In 1997 Rose Valley Chorus &amp; Orchestra purchased the old Middletown Firehouse in Media. It serves as our set construction site and houses our extensive costume and prop collection.</p>
                   <p>In keeping with its long-standing traditions and policies, Rose Valley Chorus &amp; Orchestra considers those seeking to participate in its programming and productions on the basis of individual merit.  RVCO does not discriminate on the basis of race, color, religion, sex, sexual orientation, gender identity, national or ethnic origin, age, status as an individual with a disability, protected veteran status, genetic information, or other protected classes under the law.</p>
                 </div>
-                <div class="col-lg-4 d-none d-lg-block text-center">
+                <div class="col-lg-5 d-none d-lg-block text-center">
                   <img src="/images/logo/red RVCO Logo1907 transparent@500.webp" alt="" name="Picture8" id="rvcoLogo" class="img-fluid" />
                 </div>
               </div>
