d46404120049bfebe77072b08f5cd640689de285
johnnysheps <johnsheppard281@gmail.com>
2025-10-02 16:43:27 -0400

    fall update 2

--- fall_show.php
+++ fall_show.php
@@ -14,48 +14,77 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center">
-                        <img src="/images/Gondoliers logo@500w.webp" alt="Gondoliers" class="img-fluid" name="gondoliers" />
+                        <div class="row">
+                            <img src="/images/Gondoliers logo@500w.webp" alt="Gondoliers" class="img-fluid" name="gondoliers" />
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <strong>Synopsis</strong>
+                                <p>
+                                    Marco and Giuseppe Palmieri, Venetian gondoliers, are told that one of them is the heir to the throne of Barataria. Until it is 
+                                    determined which one is the new king, the brothers must rule together. Complicating matters, the brothers have not only recently
+                                    married young Venetian ladies, but they will soon meet the daughter of the Duke of Plaza Toro to whom the unsuspecting future 
+                                    king was married in infancy. As the Grand Inquisitor seeks the truth from the prince’s foster mother, everyone assembles in 
+                                    Barataria awaiting the answer. Who is the rightful king?…and will hearts be mended or broken?
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center">
-                        <strong>Synopsis</strong>
-                        <p>
-                            Marco and Giuseppe Palmieri, Venetian gondoliers, are told that one of them is the heir to the throne of Barataria. Until it is 
-                            determined which one is the new king, the brothers must rule together. Complicating matters, the brothers have not only recently
-                             married young Venetian ladies, but they will soon meet the daughter of the Duke of Plaza Toro to whom the unsuspecting future 
-                             king was married in infancy. As the Grand Inquisitor seeks the truth from the prince’s foster mother, everyone assembles in 
-                             Barataria awaiting the answer. Who is the rightful king?…and will hearts be mended or broken?
-                        </p>
+                        <div class="row">
+                            <div class="col-12">
+                                <p>Book by W. S. Gilbert</p>
+                                <p>Music by Arthur Sullivan</p>
+                                <br/>
+                                <p>Katherine P. Nealley - <em>Stage Director</em></p>
+                                <p>Florrie Marks - <em>Music Director</em></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <strong>Show Times</strong>
+                                <p>Saturday, November 8 at 2 p.m. and 7:30 p.m.</p>
+                                <p>Sunday, November 9 at 2 p.m.</p>
+                                <p>Wednesday, November 12 at 7:30 p.m.</p>
+                                <p>Saturday, November 15 at 2 p.m. and 7:30 p.m.</p>
+                                <p>Sunday, November 16 at 2 p.m.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                Strath Haven Middle School<br/>
+                                Wallingford, PA
+                            </div>
+                        </div>
+                        
 	                </div>
                 </div>
 
                 <div class="row">
                     <div class="container marketing">
-                    <div class="row text-center donateSec">
-                         <span><strong>Please consider helping us keep live musical theater in the community</strong></span>
+                        <div class="row fade-in-up">
+                            <div class="col-lg-3 text-center mb-3">
+                                <img class="rounded-circle" width="140" height="140" src="/images/ticketsIcon.png">
+                                <h2>Buy Tickets</h2>
+                                <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/the-gondoliers-tickets">Buy Tickets</a></p>
+                            </div>
+                            <div class="col-lg-3 text-center mb-3">
+                                <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
+                                <h2>Sponsor</h2>
+                                <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
+                            </div>
+                            <div class="col-lg-3 text-center mb-3">
+                                <img class="rounded-circle" width="140" height="140" src="/images/adsIcon.png">
+                                <h2>PlayBill Ads</h2>
+                                <p><a class="btn btn-info" href="https://rvco.org/Ads.php">Playbill Ads</a></p>
+                            </div>
+                            <div class="col-lg-3 text-center mb-3">
+                                <img class="rounded-circle" width="140" height="140" src="/images/castIcon.png">
+                                <h2>Cast</h2>
+                                <p><a class="btn btn-info" href="https://rvco.org/gondoliers/GONDOLIERS25cast.php">Cast</a></p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row fade-in-up">
-                         <div class="col-lg-3 text-center mb-3">
-                              <img class="rounded-circle" width="140" height="140" src="/images/ticketsIcon.png">
-                              <h2>Buy Tickets</h2>
-                              <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/the-gondoliers-tickets">Buy Tickets</a></p>
-                         </div>
-                         <div class="col-lg-3 text-center mb-3">
-                              <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
-                              <h2>Sponsor</h2>
-                              <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
-                         </div>
-                         <div class="col-lg-3 text-center mb-3">
-                              <img class="rounded-circle" width="140" height="140" src="/images/adsIcon.png">
-                              <h2>PlayBill Ads</h2>
-                              <p><a class="btn btn-info" href="https://rvco.org/Ads.php">Playbill Ads</a></p>
-                         </div>
-                         <div class="col-lg-3 text-center mb-3">
-                              <img class="rounded-circle" width="140" height="140" src="/images/castIcon.png">
-                              <h2>Cast</h2>
-                              <p><a class="btn btn-info" href="https://rvco.org/gondoliers/GONDOLIERS25cast.php">Cast</a></p>
-                         </div>
-                    </div>
-               </div>
                 </div>
             </div>
           </main>
