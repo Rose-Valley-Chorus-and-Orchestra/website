ca47036ed0f836b16b56704e840a471d86241db3
johnnysheps <johnsheppard281@gmail.com>
2025-10-02 22:29:45 -0400

    shows structuring

--- fall_show.php
+++ fall_show.php
@@ -4,97 +4,11 @@
     <body>
         <?php include 'includes/header.php'; ?>
 
-        <!-- Main content -->
-        <main>
-            <div class="container">
-                <div class="row mt-3 slide-in-right">
-                    <div class="col-12 text-end">
-                        <h1 class="display-3">The Gondoliers</h1>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center ticketImg">
-                        <div class="row">
-                            <img src="/images/Gondoliers logo@500w.webp" alt="Gondoliers" class="img-fluid" name="gondoliers" />
-                        </div>
-                        <div class="row mt-4 ">
-                            <div class="col-12">
-                                <h4><strong>Synopsis</strong></h4>
-                                <p>
-                                    Marco and Giuseppe Palmieri, Venetian gondoliers, are told that one of them is the heir to the throne of Barataria. Until it is 
-                                    determined which one is the new king, the brothers must rule together. Complicating matters, the brothers have not only recently
-                                    married young Venetian ladies, but they will soon meet the daughter of the Duke of Plaza Toro to whom the unsuspecting future 
-                                    king was married in infancy. As the Grand Inquisitor seeks the truth from the prince’s foster mother, everyone assembles in 
-                                    Barataria awaiting the answer. Who is the rightful king?…and will hearts be mended or broken?
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center ticketImg">
-                        <div class="row">
-                            <div class="col-12">
-                                <h4>Book by W. S. Gilbert</h4>
-                                <h4>Music by Arthur Sullivan</h4>
-                                <br/>
-                                <h4>Katherine P. Nealley - <em>Stage Director</em></h4>
-                                <h4>Florrie Marks - <em>Music Director</em></h4>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <h3><strong>Show Times</strong></h3>
-                                <h5>Saturday, November 8 at 2 p.m. and 7:30 p.m.</h5>
-                                <h5>Sunday, November 9 at 2 p.m.</h5>
-                                <h5>Wednesday, November 12 at 7:30 p.m.</h5>
-                                <h5>Saturday, November 15 at 2 p.m. and 7:30 p.m.</h5>
-                                <h5>Sunday, November 16 at 2 p.m.</h5>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <h3><strong>Venue</strong></h3>
-                                <h5>
-                                    Strath Haven Middle School<br/>
-                                    Wallingford, PA
-                                </h5>
-                            </div>
-                        </div>
-                        
-	                </div>
-                </div>
+        <!-- swap this out for each show in the fall -->
+        <?php include '/shows/gondoliers.php'; ?>
 
-                <div class="row mt-4">
-                    <div class="container marketing">
-                        <div class="row fade-in-up">
-                            <div class="col-lg-3 text-center mb-3">
-                                <img class="rounded-circle" width="140" height="140" src="/images/ticketsIcon.png">
-                                <h2>Buy Tickets</h2>
-                                <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/the-gondoliers-tickets">Buy Tickets</a></p>
-                            </div>
-                            <div class="col-lg-3 text-center mb-3">
-                                <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
-                                <h2>Sponsor</h2>
-                                <p><a class="btn btn-info" href="https://www.zeffy.com/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
-                            </div>
-                            <div class="col-lg-3 text-center mb-3">
-                                <img class="rounded-circle" width="140" height="140" src="/images/adsIcon.png">
-                                <h2>PlayBill Ads</h2>
-                                <p><a class="btn btn-info" href="https://rvco.org/Ads.php">Playbill Ads</a></p>
-                            </div>
-                            <div class="col-lg-3 text-center mb-3">
-                                <img class="rounded-circle" width="140" height="140" src="/images/castIcon.png">
-                                <h2>Cast</h2>
-                                <p><a class="btn btn-info" href="https://rvco.org/gondoliers/GONDOLIERS25cast.php">Cast</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          </main>
+        <?php include 'includes/footer.php'; ?>
 
-          <!-- Footer -->
-          <?php include 'includes/footer.php'; ?>
-
-          <?php include 'includes/scripts.php'; ?>
+        <?php include 'includes/scripts.php'; ?>
      </body>
 </html>
