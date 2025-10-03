e7c315ef1ed213dc4d7801a1b042465494a92596
johnnysheps <johnsheppard281@gmail.com>
2025-10-03 10:35:05 -0400

    contact us

---
+++ contacts.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+    <?php include 'includes/head.php'; ?>
+     <body>
+        <?php include 'includes/header.php'; ?>
+
+          <!-- Main content -->
+        <main>
+            <div class="container">
+                <div class="row mt-3 slide-in-right">
+                    <div class="col-12 text-end">
+                        <h1 class="display-3">Contact Us</h1>
+                    </div>
+                </div>
+                <div class="row mt-4 mb-4">
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 ticketImg mb-4">
+                        <h4>E-Mail</h4>
+                        <ul class="list-unstyled">
+                            <li>For general inquiries: <a href="mailto:info@rvco.org">info@rvco.org</a></li>
+                            <li>For tickets: <a href="mailto:tickets@rvco.org">tickets@rvco.org</a></li>
+                            <li>To join the chorus, orchestra, production crew, or to audition for a leading role: <a href="mailto:auditions@rvco.org">auditions@rvco.org</a></li>
+                            <li>To inquire about a RVCO roadshow for your organization: <a href="mailto:roadshows@rvco.org">roadshows@rvco.org</a></li>
+                            <li>To join our group or for questions about membership: <a href="mailto:members@rvco.org">members@rvco.org</a></li>
+                            <li>To become a sponsor: <a href="mailto:sponsorship@rvco.org">sponsorship@rvco.org</a></li>
+                            <li>To place an ad in our playbill: <a href="mailto:ads@rvco.org">ads@rvco.org</a></li>
+                            <li>To connect with our marketing/PR committee: <a href="mailto:marketing@rvco.org">marketing@rvco.org</a></li>
+                            <li>For issues concerning this site: <a href="mailto:webmaster@rvco.org">webmaster@rvco.org</a></li>
+                        </ul>
+                        <h4>Mailing List</h4>
+                        <p>
+                            If you would like to be added or removed from our mailing list, email and post mail, email us at <a href="mailto:info@rvco.org?subject=Mailing List">info@rvco.org</a>.
+                            Please include your name, address, and email address.</p>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                        <h4>Mailing Address</h4>
+                        <p>
+                            Rose Valley Chorus and Orchestra<br>
+                            P.O. Box 414<br>
+                            Media, PA 19063
+                        </p> 
+                        <h4>Phone</h4>
+                        <p>
+                            General Number: 610-565-5010
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <!-- Footer -->
+        <?php include 'includes/footer.php'; ?>
+
+        <?php include 'includes/scripts.php'; ?>
+     </body>
+
+     </body>
+</html>
