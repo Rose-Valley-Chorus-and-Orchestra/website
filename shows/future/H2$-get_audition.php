ba9e0b3758fb544974c023c940c9352939e63e0d
johnnysheps <johnsheppard281@gmail.com>
2025-10-07 16:21:21 -0400

    file restructure

---
+++ shows/future/H2$-get_audition.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>How to Succeed Download Audition Material - RVCO</title>
+<link rel="shortcut icon" href="../images/rvco.ico" />
+<link href="../css/bootstrap.css" rel="stylesheet" type="text/css">	
+<link href="../css/multiColumnTemplate.css" rel="stylesheet" type="text/css">
+<link href="../css/multiColumn_Max425.css" rel="stylesheet" type="text/css">
+<link href="../css/multiColumn_Min426Max768.css" rel="stylesheet" type="text/css">
+<link href="../css/multiColumn_Min769Max1000.css" rel="stylesheet" type="text/css">
+<link href="../css/multiColumn_Min1001.css" rel="stylesheet" type="text/css">
+
+<!-- First uploaded September 16, 2025 -->
+
+<script type="text/javascript">
+
+function MM_showHideLayers() { //v9.0
+  var i,p,v,obj,args=MM_showHideLayers.arguments;
+  for (i=0; i<(args.length-2); i+=3) 
+  with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
+    if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+    obj.visibility=v; }
+}
+function MM_changeProp(objId,x,theProp,theValue) { //v9.0
+  var obj = null; with (document){ if (getElementById)
+  obj = getElementById(objId); }
+  if (obj){
+    if (theValue == true || theValue == false)
+      eval("obj.style."+theProp+"="+theValue);
+    else eval("obj.style."+theProp+"='"+theValue+"'");
+  }
+}
+
+</script>
+    
+</head>
+<body>
+<div class="container">
+  <header>
+    <div class="primary_header">
+		 <?php include("../header2_new.php"); ?>
+    </div>
+ </header>
+
+    
+<!--begin content -->     
+<div id="content" style="padding: 30px 10px;">
+  <h1 id="pageName">AUDITION MATERIAL</h1>
+      <div id="auditions" class="story">
+          <p>To download your audition material, click on the name of the character.</p>
+          <p><b>Note:</b> the music has been provided with and without the piano accompaniment.</p>
+
+               <!--<div><p style="border: thin #666666 solid">Please return to see more information regarding auditions for <i>Me and My Girl</i>.  RVCO is tentatively planning on holding auditions at the end of Septmber and the beginning of October.</p>
+               </div>-->
+             <div class="story">
+
+                     <div style="padding: 10px 10px 3px 20px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Finch.pdf">J. Pierrepont Finch</a></b>
+                         <p>Sing <u>one</u> of the following:
+                              <ul style="list-style-type: disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                 <li><i>I Believe in You</i> : pick-up to C through 7 measures after E (or 2 measures before F)</li>
+                                 <li><i>Brotherhood of Man</i> : 2 measures before C to G</li></ul></p>
+                         <p>Read: p.4 Biggley's "You heard me" to p.6 "Yes sir, he told me to see you"</p>
+                         </div>
+                    
+
+                    <div style="padding: 10px 10px 3px 20px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Bud.pdf">Bud Frump</a></b>
+                         <p>Sing <u>one</u> of the following:
+                              <ul style="list-style-type: disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                 <li><i>I Believe in You</i> : pick-up to C through 7 measures after E (or 2 measures before F)</li>
+                                 <li><i>Brotherhood of Man</i> : 2 measures before C to G</li></ul></p>
+                         <p>Read: p.77 "going to the party" to end of p.78</p>
+                         </div>
+                  
+                  
+                         <div style="padding: 10px 10px 3px 20px;  margin-top: 0px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Biggley.pdf">J. B. Biggley</a></b>
+                              <p>Sing <u>one</u> of the following:                              
+                                   <ul style="list-style-type: disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                        <li><i>Grand Old Ivy</i> : measure 9 to 1 measure before E</li>
+                                        <li><i>Brotherhood of Man</i> : 2 measures before C to G</li></ul></p>
+                                 <p>Read: p. 39 exact whole page</p>
+                         </div>
+
+                                <div style="padding: 10px 10px 3px 20px; margin-bottom: 10px;">If you are auditioning for <b style="font-size: 1.5em;">Twimble/Womper or Gatch</b> do the singing and reading required for J. B. Bigley.</div>
+       
+       
+                         <div style="padding: 10px 10px 3px 20px;  margin-top: 0px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Bratt.pdf">Bratt</a></b>
+                              <p>Sing <u>one</u> of the following:                              
+                                   <ul style="list-style-type:disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                        <li><i>Grand Old Ivy</i> : measure 9 to 1 measure before E</li>
+                                        <li><i>Brotherhood of Man</i> : 2 measures before C to G</li></ul></p>
+                                 <p>Read: p.30 "Hello. Yes, JB." to end of 31</p>
+                         </div>
+
+                         <div style="padding: 10px 10px 3px 20px; margin-top: 0px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Rosemary.pdf">Rosemary</a></b>
+                              <p>Sing <u>one</u> of the following:                              
+                                   <ul style="list-style-type:disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                      <li><i>Happy to Keep His Dinner Warm</i> : pick-up to E to the end</li>
+                                        <li><i>Paris Original</i> : E to 5 measures after I</li></ul></p>
+                              <p>Read: p.90-91: p.90 "Rosemary?" To p.91 "… in gold leaf."</p>
+                         </div>
+
+                         <div style="padding: 10px 10px 3px 20px; margin-top: 0px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Smitty.pdf">Smitty</a></b>
+                              <p>Sing <u>one</u> of the following:                              
+                                   <ul style="list-style-type:disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                      <li><i>Happy to Keep His Dinner Warm</i> : pick-up to E to the end</li>
+                                        <li><i>Paris Original</i> : E to 5 measures after I</li></ul></p>
+                              <p>Read: p.7 (with Rosemary) "Good God, Rosemary" to end of page</p>
+                         </div>
+
+                         <div style="padding: 10px 10px 3px 20px; margin-top: 0px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Hedy.pdf">Hedy</a></b>
+                              <p>Sing <u>one</u> of the following:                              
+                                   <ul style="list-style-type:disc; margin: -10px 0px -20px; padding-left: 60px;">
+                                      <li><i>Happy to Keep His Dinner Warm</i> : pick-up to E to the end</li>
+                                        <li><i>Paris Original</i> : E to 5 measures after I</li></ul></p>
+                              <p>Read: p.53 (w Biggley): "how do you like your" to "but angel"</p>
+                         </div>
+
+                         <div style="padding: 10px 10px 3px 20px; margin-bottom: 0px;"><b style="font-size: 1.5em;"><a href="Audition material/Miss Jones.pdf">Miss Jones</a></b>
+                              <ul style="list-style-type:disc; margin: 10px 0px -20px; padding-left: 60px;">
+                                   <li>Sing <i>Brotherhood of Man</i>: Q to S</li>
+                                   <li>Read: p.18 -19: "Pardon me, ma'am" to end of p.19</li>
+                         </div>
+          
+
+
+          </div>   <!--end story -->
+    
+
+     </div>   <!-- id="auditions" class="story" -->
+</div>    <!--end content -->
+
+
+</div>  <!-- The end of id="container"  -->
+</body>
+</html>
