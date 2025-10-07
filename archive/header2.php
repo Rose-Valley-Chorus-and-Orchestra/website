a7d95cfc417fc60b3049a03107afc89c595979b6
johnnysheps <johnsheppard281@gmail.com>
2025-10-06 23:53:12 -0400

    cleanup repo

--- archive/header2.php
+++
@@ -1,5 +0,0 @@
-<div id="masthead">  <div id="facebook_youtube"><a href="http://www.facebook.com/pages/Media-PA/Rose-Valley-Chorus-Orchestra-RVCO/143047578480?ref=nf" target="_blank"><img src="/images/facebook.svg" alt="Find us on Facebook" width="20" height="20" vspace="3" align="absmiddle" title="Find us on Facebook" /></a> <a href="http://www.facebook.com/pages/Media-PA/Rose-Valley-Chorus-Orchestra-RVCO/143047578480?ref=nf">Find us on facebook</a><br />
-    <a href="http://www.youtube.com/user/rosevalleychorus" target="_blank"><img src="/images/youtube.svg" alt="Visit our YouTube channel" width="20" height="20" vspace="1" align="absmiddle" title="Visit our YouTube channel" /></a> <a href="http://www.youtube.com/user/rosevalleychorus">Visit our YouTube channel</a></div>
-  <h1 id="siteName"><a href="file:///Seagate X/index.php">
-Rose Valley Chorus &amp; Orchestra</a></h1>
-</div>
