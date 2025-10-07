433a59922d3c07f5c615fc6ee42dd8aa10298528
johnnysheps <johnsheppard281@gmail.com>
2025-10-07 14:45:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
 <meta name="keywords" content="The Pirates of Penzance, Me and My Girl, Rose Valley, Delaware Valley, theater, theatre, performance, orchestra, Rose Valley, Media, Springfield" />
 <title>Rose Valley Chorus and Orchestra</title>
 <link rel="shortcut icon" href="images/rvco.ico" />
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">	
-<link href="css/multiColumnTemplate.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+<!-- <link href="css/bootstrap.css" rel="stylesheet" type="text/css">	 -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+ <link href="css/multiColumnTemplate.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
 <link href="css/multiColumn_Max425.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
 <link href="css/multiColumn_Min426Max768.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
 <link href="css/multiColumn_Min769Max1000.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
