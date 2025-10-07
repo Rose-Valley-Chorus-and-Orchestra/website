27922be480201b2ee84bdf38b881f918e391ed39
johnnysheps <johnsheppard281@gmail.com>
2025-10-06 23:18:26 -0400

    update sandbox

---
+++ past_shows/fairlady/audition material/Auditions_fairlady.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Rose Valley Chorus &amp; Orchestra</title>
+<link rel="stylesheet" href="../../../archive/stylesheet/2col_leftNav.css" type="text/css" />
+<link rel="shortcut icon" href="../../../images/rvco.ico" /></head>
+<body>
+<?php include("../../../header.php"); ?>
+
+<!-- end masthead -->
+<div id="content">
+  <h2 id="pageName">AUDITIONS </h2>
+  <div class="story">
+    <div align="justify">The roles for <b>My Fair Lady</b> have been cast.  If you are interested in the chorus please <a href="mailto:auditions@rvco.org">e-mail</a> with your name, phone number, and best time to reach you. Rehearsals start late January 2007. The show goes up April 20, 21, 25, 27, &amp; 28 2012.</div>
+  </div>
+  <div class="story">
+    <div align="justify">The shows for next season have not been decided. Please check back.</div>
+  </div>
+</div>
+<!--end content -->
+<div id="navBar">
+  <div id="sectionLinks" role="navigation">
+    <ul>
+      <li><a href="../../../index.php">Current Season</a></li>
+      <li><a href="../../../AboutUs.php">About Us</a></li>
+      <li><a href="../../../Tickets.php">Tickets</a></li>
+      <li><a href="../../../archive/Venue.php">Venue</a></li>
+      <li><a href="../../../archive/Join Us.php">Join Us </a></li>
+      <li><a href="../../../archive/Review.php">Reviews</a></li>
+      <li><a href="../../../Contacts.php">Contact Us</a></li>
+      <li><a href="../../../archive/Dining.php">Dining Options</a></li>
+      <li class="active">Audition Information</li>
+      <li><a href="../../../archive/Music.php">Music in Schools</a></li>
+	  <li><a href="../../../archive/CPAC.php">Performing Arts Complex<br />of Delaware County</a></li>
+</ul>
+  </div>
+  <a href="file:///Daisy/Users/Windsor/Documents/index.html"><img src="../../../images/RVCO_small.gif" alt="Logo" name="logo" hspace="10" vspace="3" id="logo" /></a></div>
+<!--end navbar -->
+<?php include("../../../footer.php"); ?>
+</body>
+</html>
---
+++ past_shows/fairlady/fairlady_schedule.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Rose Valley Chorus &amp; Orchestra</title>
+<link rel="stylesheet" href="../../archive/stylesheet/2col_leftNav.css" type="text/css" />
+<link rel="shortcut icon" href="../../images/rvco.ico" />
+<style type="text/css">
+<!--
+.story table tr td {
+	color: #191919;
+}
+.style1 {color: #999}
+-->
+</style>
+</head>
+<body>
+ <?php include("../../header2.php"); ?>
+<div id="masthead">
+    <h2 align="center" style="text-align:center;">RVCO - <i>My Fair Lady</i> &ndash; Spring 2012<br />
+		Rehearsal Schedule &ndash; v.7 (Mar 13, 2012)</h2>
+</div>
+<!-- end masthead -->
+<div style="font-size: smaller; float:right; clear: both; padding-right: 10px"><a href="audition material/index.php" target="_top"><u> Back to Cast Page</u></a></div>
+Open other versions: <a href="MyFairLadyPreliminarySchedule.doc"><u>Microsoft Word</u><sup>&reg;</sup></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="MyFairLadyPreliminarySchedule.pages"><u>Apple Pages</u><sup>&reg;</sup></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="MyFairLadyPreliminarySchedule.pdf"><u>PDF</u></a>
+<div class="story">
+    
+<table  border="1" cellpadding="2" cellspacing="0" bgcolor="#FFFFFF">
+  <tr bgcolor="#99CCFF">
+    <th scope="col">Date</th>
+    <th scope="col">Time</th>
+    <th scope="col">Called</th>
+    <th scope="col">Location</th>
+  </tr>
+  <tr bgcolor="#FFFFCC">
+    <td>3/14/12</td>
+    <td>7:30 PM</td>
+    <td>Act 1 Scene 5: Higgins, Pickering, Eliza, Mrs Pearce, Doolittle, maids, footman, Butler</td>
+    <td>Stage One</td>
+  </tr>
+ <tr bgcolor="#FFFFCC">
+   <td>&nbsp;</td>
+   <td>&nbsp;</td>
+   <td>   Work Music with Ryan</td>
+   <td>&nbsp;</td>
+ </tr>
+ <tr>
+   <td>3/18/12</td>
+   <td>7:00 PM</td>
+  <td>&quot;stumble through&quot; of Act 1</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#EEEEEE">
+   <td>3/19/12</td>
+   <td>7:30 PM</td>
+  <td>Continue &quot;stumble through&quot; of Act 1 (OFF BOOK NIGHT)</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#FFFFCC">
+   <td>3/21/12</td>
+   <td>7:30 PM</td>
+  <td>&quot;stumble through&quot; of Act 2</td>
+   <td>Stage One</td>
+ </tr>
+ <tr>
+   <td>3/25/12</td>
+   <td>7:00 PM</td>
+  <td>All Called -
+    Run Problem group scenes, 
+    Choreography</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#EEEEEE">
+   <td>3/26/12</td>
+   <td>7:30 PM</td>
+  <td>All Called - RUN ACT 1</td>
+   <td>Stage One</td>
+ </tr>
+ <tr>
+   <td bgcolor="#FFFFCC">3/28/12</td>
+   <td bgcolor="#FFFFCC">7:30 PM</td>
+  <td bgcolor="#FFFFCC">Run Act 1  (Continued)</td>
+   <td bgcolor="#FFFFCC">Stage One</td>
+ </tr>
+ <tr>
+   <td>4/1/12</td>
+   <td>7:00 PM</td>
+  <td>Run Act</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#EEEEEE">
+   <td>4/2/12</td>
+   <td>7:30 PM</td>
+  <td>Fix Problem Act I scenes<br />
+    Higgins costume parade</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#FFFFCC">
+   <td>4/4/12</td>
+   <td>7:30 PM</td>
+  <td>Costume Parade<br />
+    All called (except Higgins)</td>
+   <td>Stage One</td>
+ </tr>
+ <trs>
+ <tr bgcolor="#FFCCFF" style='mso-yfti-irow:120'>
+   <td>4/8/12</td>
+   <td>&nbsp;</td>
+   <td>Easter – all off (review your lines!)</td>
+   <td>&nbsp;</td>
+ </tr>
+ <tr bgcolor="#EEEEEE">
+   <td>4/9/12</td>
+   <td>7:30 PM</td>
+  <td>Fix Problems Act 2 -  All called</td>
+   <td>Stage One</td>
+ </tr>
+ <tr bgcolor="#FFFFCC">
+   <td>4/11/12</td>
+   <td>7:30 PM</td>
+  <td>Run</td>
+   <td>Stage One</td>
+ </tr>
+ <tr>
+  <td colspan="4" >&nbsp;&nbsp;&nbsp;&nbsp;<b>Weekend of 4/14/12 – Move in</b></td>
+  </tr>
+ <tr>
+   <td>4/15/12</td>
+   <td>2:00 – 10:00 PM</td>
+  <td>Run with Tech X2</td>
+   <td>Strath Haven M.S.</td>
+  </tr>
+ <tr>
+   <td>4/16/12</td>
+   <td>6:30 PM</td>
+  <td>Run with costumes</td>
+   <td>Strath Haven M.S.</td>
+  </tr>
+ <tr>
+   <td>4/17/12</td>
+   <td>6:30 PM</td>
+  <td>Run with costumes/make up</td>
+   <td>Strath Haven M.S.</td>
+  </tr>
+ <tr>
+   <td>4/18/12</td>
+   <td>6:30 PM</td>
+  <td>Final run</td>
+   <td>Strath Haven M.S.</td>
+  </tr>
+ <tr>
+   <td bgcolor="#EEEEEE">4/20/12</td>
+   <td bgcolor="#EEEEEE">6:00 PM call </td>
+   <td bgcolor="#EEEEEE">Show</td>
+   <td bgcolor="#EEEEEE">Strath Haven M.S.</td>
+  </tr>
+<tr>
+   <td bgcolor="#EEEEEE">4/21/12</td>
+   <td bgcolor="#EEEEEE">12:00 PM call</td>
+  <td bgcolor="#EEEEEE">Show</td>
+   <td bgcolor="#EEEEEE">Strath Haven M.S.</td>
+ </tr> <tr>
+   <td bgcolor="#EEEEEE">4/21/12</td>
+   <td bgcolor="#EEEEEE">6:00 PM call</td>
+  <td bgcolor="#EEEEEE">Show</td>
+   <td bgcolor="#EEEEEE">Strath Haven M.S.</td>
+ </tr>
+ 
+ <tr>
+   <td bgcolor="#FFFFCC">4/25/12</td>
+   <td bgcolor="#FFFFCC">6:00 PM call</td>
+  <td bgcolor="#FFFFCC">Show</td>
+   <td bgcolor="#FFFFCC">Strath Haven M.S.</td>
+ </tr>
+ <tr>
+   <td bgcolor="#FFFFCC">4/27/12</td>
+   <td bgcolor="#FFFFCC">6:00 PM call</td>
+  <td bgcolor="#FFFFCC">Show</td>
+   <td bgcolor="#FFFFCC">Strath Haven M.S.</td>
+ </tr>
+ <tr >
+   <td bgcolor="#FFFFCC">4/28/12</td>
+   <td bgcolor="#FFFFCC">12:00 PM call</td>
+  <td bgcolor="#FFFFCC">Show</td>
+   <td bgcolor="#FFFFCC">Strath Haven M.S.</td>
+ </tr>
+ <tr >
+   <td bgcolor="#FFFFCC">4/28/12</td>
+   <td bgcolor="#FFFFCC">6:00 PM call</td>
+  <td bgcolor="#FFFFCC">Show</td>
+   <td bgcolor="#FFFFCC">Strath Haven M.S.</td>
+ </tr>
+ <tr>
+   <td>4//29/12</td>
+   <td>TBA</td>
+  <td>ALL -  &nbsp;Break set –time</td>
+   <td>Strath Haven M.S.</td>
+ </tr>
+</table>
+    
+    
+    
+    <p><br />
+  <a href="#top">Back to top</a></p>
+</div>
+  <!--end content -->
+<!--end navbar -->
+ <?php include("../../footer.php"); ?>
+</body>
+</html>
