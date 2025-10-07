0de1c8a7d2a4162a1c2ffa4e4cab1d2587e24055
johnnysheps <johnsheppard281@gmail.com>
2025-10-06 23:00:53 -0400

    update sandbox 4

---
+++ gondoliers/audition material/buyGondoliers.php
@@ -0,0 +1,94 @@
+  <table border="1" align="center" cellpadding="2" cellspacing="0" bordercolor="#999" class="style1">
+    <tr>
+      <th valign="top">Type</th>
+      <th align="center" valign="top">Price</th>
+      <th align="center" valign="top" style="font-size:120%">&nbsp;</th>
+      <th width="15%" rowspan="6" style="background-color:#FFCCCC;color:#666;">Put the number of tickets in the Quantity column of the shopping cart on the next page.</th>
+    </tr>
+    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <tr>
+        <td valign="top">Adult</td>
+        <td align="center" valign="top">$17</td>
+        <td align="center" valign="top"><input type="image" vspace="2" src="../images/buyTickets.gif" border="0" name="submit" alt="Add to cart" title="Add to cart" />
+          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <input type="hidden" name="add" value="1" />
+          <input type="hidden" name="cmd" value="_cart" />
+          <input type="hidden" name="business" value="rosevalleychorus@gmail.com" />
+          <input type="hidden" name="item_name" value="Adult Ticket - The Gondoliers" />
+          <input type="hidden" name="item_number" value="Autumn 2014 Adult Ticket - The Gondoliers" />
+          <input type="hidden" name="amount" value="17.00" />
+          <input type="hidden" name="no_shipping" value="2" />
+          <input type="hidden" name="return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="cancel_return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="no_note" value="1" />
+          <input type="hidden" name="currency_code" value="USD" />
+          <input type="hidden" name="bn" value="PP-ShopCartBF" /></td>
+      </tr>
+    </form>
+    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <tr>
+        <td valign="top">Senior Citizen (60+) / Student</td>
+        <td align="center" valign="top">$14</td>
+        <td align="center" valign="top"><input type="image" vspace="2" src="../images/buyTickets.gif" border="0" name="submit" alt="Add to cart" title="Add to cart" />
+          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <input type="hidden" name="add" value="1" />
+          <input type="hidden" name="cmd" value="_cart" />
+          <input type="hidden" name="business" value="rosevalleychorus@gmail.com" />
+          <input type="hidden" name="item_name" value="Senior/Student Ticket - The Gondoliers" />
+          <input type="hidden" name="item_number" value="Autumn 2014 Senior/Student - The Gondoliers" />
+          <input type="hidden" name="amount" value="14.00" />
+          <input type="hidden" name="no_shipping" value="2" />
+          <input type="hidden" name="return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="cancel_return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="no_note" value="1" />
+          <input type="hidden" name="currency_code" value="USD" />
+          <input type="hidden" name="bn" value="PP-ShopCartBF" />
+          <input type="hidden" name="bn" value="PP-ShopCartBF" /></td>
+      </tr>
+    </form>
+    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <tr>
+        <td valign="top">Children<br />
+          (12 years old and under)</td>
+        <td align="center" valign="top">$7</td>
+        <td align="center" valign="top"><input type="image" vspace="2" src="../images/buyTickets.gif" border="0" name="submit" alt="Add to cart" title="Add to cart" />
+          <input type="hidden" name="add" value="1" />
+          <input type="hidden" name="cmd" value="_cart" />
+          <input type="hidden" name="business" value="rosevalleychorus@gmail.com" />
+          <input type="hidden" name="item_name" value="Child Ticket - The Gondoliers" />
+          <input type="hidden" name="item_number" value="Autumn 2014 Child Ticket - The Gondoliers" />
+          <input type="hidden" name="amount" value="7.00" />
+          <input type="hidden" name="no_shipping4" value="2" />
+          <input type="hidden" name="return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="cancel_return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="no_note" value="1" />
+          <input type="hidden" name="currency_code" value="USD" />
+          <input type="hidden" name="bn" value="PP-ShopCartBF" /></td>
+      </tr>
+    </form>
+    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <tr>
+        <td valign="top">Group<br />
+(10 or more Adult tickets<br />
+for the same performance)</td>
+        <td align="center">$12</td>
+        <td align="center" valign="top"><input type="image" vspace="2" src="../images/buyTickets.gif" border="0" name="submit" alt="Add to cart" title="Add to cart" />
+          <input type="hidden" name="add" value="1" />
+          <input type="hidden" name="cmd" value="_cart" />
+          <input type="hidden" name="business" value="rosevalleychorus@gmail.com" />
+          <input type="hidden" name="item_name" value="Group Ticket - The Gondoliers" />
+          <input type="hidden" name="item_number" value="Autumn 2014 Group Ticket - The Gondoliers" />
+          <input type="hidden" name="amount" value="12.00" />
+          <input type="hidden" name="no_shipping" value="2" />
+          <input type="hidden" name="return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="cancel_return" value="http://www.rvco.org/Tickets.html" />
+          <input type="hidden" name="no_note" value="3" />
+          <input type="hidden" name="currency_code" value="USD" />
+        <input type="hidden" name="bn5" value="PP-ShopCartBF" /></td>
+      </tr>
+    </form>
+    <tr><td colspan="3"><span class="style2">Tickets ordered online must be picked up at the theater. Please print out the PayPal receipt and bring it to the theater.</span></td></tr>
+  </table>
+<div align="center">
+  <a href="#" onclick="javascript:window.open('https://www.paypal.com/us/cgi-bin/webscr?cmd=xpt/popup/OLCWhatIsPayPal-outside','olcwhatispaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=350');"><img  src="../images/horizontal_solution_PP.gif" alt="Solution Graphics" width="157" height="60" border="0" /></a>
+</div>
