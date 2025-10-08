f3d4bad687640b6858eb21188ec4580b09c4ea14
johnnysheps <johnsheppard281@gmail.com>
2025-10-08 08:45:16 -0400

    navigation template

---
+++ includes/navigation.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="secondary_header" id="menu">
+      <ul> 
+            <li>HOME</li>
+            <li><a href="AboutUs.php">ABOUT</a></li>
+            <li><a href="Tickets.php">TICKETS</a></li>
+		  <li><a href="Venue.php">VENUE</a></li>
+		  <li><a href="Auditions.php">AUDITIONS</a></li>
+           <li><a href="Contacts.php"><nobr>CONTACT US</nobr></a></li>
+      </ul>
+     </div>
+</nav>
