1d69f3185a82a0f9a561fd3ab7cd8ba31aaf8319
johnnysheps <johnsheppard281@gmail.com>
2025-10-08 09:15:58 -0400

    head include

---
+++ includes/head.php
@@ -0,0 +1,51 @@
+<head>
+    <meta charset="UTF-8">
+    <!--<meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="William Michael">
+    <meta name="viewport" content="width=device-width, initial-scale=1">     
+    <meta name="description" content="We are a community theater troupe that has been putting on two shows a year with a full orchestra for over 100 years, performance are in the spring and fall" />
+    <meta name="keywords" content="The Pirates of Penzance, Me and My Girl, Rose Valley, Delaware Valley, theater, theatre, performance, orchestra, Rose Valley, Media, Springfield" />
+    <title>Rose Valley Chorus and Orchestra</title>
+    <link rel="shortcut icon" href="images/rvco.ico" />
+    <!-- <link href="css/bootstrap.css" rel="stylesheet" type="text/css">	 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link href="css/multiColumnTemplate.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+    <link href="css/multiColumn_Max425.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+    <link href="css/multiColumn_Min426Max768.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+    <link href="css/multiColumn_Min769Max1000.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+    <link href="css/multiColumn_Min1001.css?v<?php echo date('his'); ?>" rel="stylesheet" type="text/css">
+    <script src="//use.edgefonts.net/calligraffitti;chewy;handlee;indie-flower.js"></script> 
+        
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    <script type="text/javascript">
+        <!--
+        function MM_showHideLayers() { //v9.0
+        var i,p,v,obj,args=MM_showHideLayers.arguments;
+        for (i=0; i<(args.length-2); i+=3) 
+        with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
+            if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+            obj.visibility=v; }
+        }
+        function MM_changeProp(objId,x,theProp,theValue) { //v9.0
+        var obj = null; with (document){ if (getElementById)
+        obj = getElementById(objId); }
+        if (obj){
+            if (theValue == true || theValue == false)
+            eval("obj.style."+theProp+"="+theValue);
+            else eval("obj.style."+theProp+"='"+theValue+"'");
+        }
+        }
+        //-->
+    </script>
+     
+</head>
