4db83fb8c0a374e88ad36ef3dc6a98d81ebc72cc
johnnysheps <johnsheppard281@gmail.com>
2025-10-09 10:03:34 -0400

    index reorder

--- index.php
+++ index.php
@@ -6,70 +6,67 @@
 
           <!-- Main content -->
           <main>
-               <div class="row mt-4 mx-3 fallShow">
-                    <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 text-end showTxt my-auto">
-                         <h3>November 2025</h3>
-                         <p>
-                              Marco and Giuseppe Palmieri, Venetian gondoliers, are told that one of them is the heir to the throne of Barataria. Until it is 
-                              determined which one is the new king, the brothers must rule together. Complicating matters, the brothers have not only recently
-                              married young Venetian ladies, but they will soon meet the daughter of the Duke of Plaza Toro to whom the unsuspecting future 
-                              king was married in infancy. As the Grand Inquisitor seeks the truth from the prince’s foster mother, everyone assembles in 
-                              Barataria awaiting the answer. Who is the rightful king?…and will hearts be mended or broken?
-                         </p>
-                         <a class="btn btn-info" role="button" aria-disabled="true" href="/fall_show.php">Learn More</a>
+               <div class="container-fluid">
+                    <div class="row mt-4 mx-3 fallShow">
+                         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 text-end showTxt my-auto">
+                              <h3>November 2025</h3>
+                              <p>
+                                   Marco and Giuseppe Palmieri, Venetian gondoliers, are told that one of them is the heir to the throne of Barataria. Until it is 
+                                   determined which one is the new king, the brothers must rule together. Complicating matters, the brothers have not only recently
+                                   married young Venetian ladies, but they will soon meet the daughter of the Duke of Plaza Toro to whom the unsuspecting future 
+                                   king was married in infancy. As the Grand Inquisitor seeks the truth from the prince’s foster mother, everyone assembles in 
+                                   Barataria awaiting the answer. Who is the rightful king?…and will hearts be mended or broken?
+                              </p>
+                              <a class="btn btn-info" role="button" aria-disabled="true" href="/fall_show.php">Learn More</a>
+                         </div>
+                         <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 text-center ticketImg">
+                              <img src="/images/Gondoliers logo@500w.webp" alt="Gondoliers" class="img-fluid" name="gondoliers" />
+                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 text-center ticketImg">
-                         <img src="/images/Gondoliers logo@500w.webp" alt="Gondoliers" class="img-fluid" name="gondoliers" />
+                    <div class="row mt-4 mx-3">
+                         <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 text-left ticketImg">
+                              <img src="/images/FLABhowToChair@500_1.webp.png" alt="H2SBusiness" class="img-fluid" name="H2SBusiness" />
+                         </div>
+                         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 text-start showTxt my-auto">
+                              <h3>Spring 2026</h3>
+                              <p>
+                                   How to Succeed in Business Without Really Trying, the Pulitzer Prize and Tony Award-winning musical, is a story 
+                                   about the triumph of the common man amid the buffoonery of big business. When an ambitious window washer named 
+                                   J. Pierrepont Finch happens upon a book entitled 'How to Succeed in Business Without Really Trying', he decides 
+                                   to begin his rise up the corporate ladder. With the book's instructions, Finch starts by landing a job in the 
+                                   mailroom. He is soon promoted and outsmarts his rival, Frump, the boss's nephew, before finally reaching the very 
+                                   top of the organization. From the beginning, Finch has the love and support of Rosemary, a marriage-minded 
+                                   secretary who finds him endearing. In his pursuit of the top job, Finch is in danger losing Rosemary's love. Finch's 
+                                   manual to success can no longer help him as he must rely on his wit and heart to save the day.
+                              </p>
+                              <a class="btn btn-info" role="button" aria-disabled="true" href="/spring_show.php">Learn More</a>
+                         </div>
                     </div>
-               </div>
-               <div class="row mt-4 mx-3">
-                    <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 text-left ticketImg">
-                         <img src="/images/FLABhowToChair@500_1.webp.png" alt="H2SBusiness" class="img-fluid" name="H2SBusiness" />
-                    </div>
-                    <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 text-start showTxt my-auto">
-                         <h3>Spring 2026</h3>
-                         <p>
-                              How to Succeed in Business Without Really Trying, the Pulitzer Prize and Tony Award-winning musical, is a story 
-                              about the triumph of the common man amid the buffoonery of big business. When an ambitious window washer named 
-                              J. Pierrepont Finch happens upon a book entitled 'How to Succeed in Business Without Really Trying', he decides 
-                              to begin his rise up the corporate ladder. With the book's instructions, Finch starts by landing a job in the 
-                              mailroom. He is soon promoted and outsmarts his rival, Frump, the boss's nephew, before finally reaching the very 
-                              top of the organization. From the beginning, Finch has the love and support of Rosemary, a marriage-minded 
-                              secretary who finds him endearing. In his pursuit of the top job, Finch is in danger losing Rosemary's love. Finch's 
-                              manual to success can no longer help him as he must rely on his wit and heart to save the day.
-                         </p>
-                         <a class="btn btn-info" role="button" aria-disabled="true" href="/spring_show.php">Learn More</a>
-                    </div>
-               </div>
 
-               
-               <div class="row">
-                    <div class="col-lg-12">
-                         <!-- Marketing / donate & sponsor -->
-                         <div class="container marketing">
+                    
+                    <div class="row">
+                         <div class="col-lg-12">
                               <div class="row text-center donateSec">
                                    <span><strong>Please consider helping us keep live musical theater in the community</strong></span>
                               </div>
                          </div>
                     </div>
-               </div>
-               
-               <div class="row fade-in-up">
-                    <div class="container marketing">
-                         <div class="col-lg-6 text-center mb-3">
+                    
+                    <div class="row fade-in-up">
+                         <div class="col-lg-6 col-md-12 text-center mb-3">
                               <img class="rounded-circle" width="140" height="140" src="/images/donateIcon.png">
                               <h2>Donate</h2>
                               <h5>Consider making a donation now</h5>
                               <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/donation-form/d9b4ba19-0df8-4869-9aa8-2b7835686f90">Donate</a></p>
                          </div>
-                         <div class="col-lg-6 text-center mb-3">
+                         <div class="col-lg-6 col-md-12 text-center mb-3">
                               <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
                               <h2>Sponsor</h2>
                               <h5>Consider becoming a Sponsor for a specific show</h5>
                               <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
                          </div>
-                    </div>
-               </div>               
+                    </div>  
+               </div>   
           </main>
 
           <!-- Footer -->
