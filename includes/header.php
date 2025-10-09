3058a20d9311457d95f6423cc3be55cd750d4bb5
johnnysheps <johnsheppard281@gmail.com>
2025-10-08 22:25:38 -0400

    css

--- includes/header.php
+++ includes/header.php
@@ -10,12 +10,12 @@
 
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='index.php'){echo 'active';} ?>" href="index.php">Home</a></li>
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='about.php'){echo 'active';} ?>" href="about.php">About Us</a></li>
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='tickets.php'){echo 'active';} ?>" href="tickets.php">Tickets</a></li>
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='venue.php'){echo 'active';} ?>" href="venue.php">Venue</a></li>
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='auditions.php'){echo 'active';} ?>" href="auditions.php">Auditions</a></li>
-          <li class="nav-item"><a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='contact.php'){echo 'active';} ?>" href="contact.php">Contact Us</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='index.php'){echo 'active';} ?>" href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='about.php'){echo 'active';} ?>" href="about.php">About Us</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='tickets.php'){echo 'active';} ?>" href="tickets.php">Tickets</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='venue.php'){echo 'active';} ?>" href="venue.php">Venue</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='auditions.php'){echo 'active';} ?>" href="auditions.php">Auditions</a></li>
+          <li class="nav-item"><a class="nav-link nvLnk <?php if(basename($_SERVER['PHP_SELF'])=='contact.php'){echo 'active';} ?>" href="contact.php">Contact Us</a></li>
         </ul>
         <form class="d-flex">
           <button class="btn btn-outline-success" type="button" id="loginBtn">Log In</button>
