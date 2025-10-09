57a88fbb062119caf3c48d9056d0dec02454c64b
johnnysheps <johnsheppard281@gmail.com>
2025-10-09 09:32:33 -0400

    main

--- index.php
+++ index.php
@@ -42,31 +42,34 @@
                     </div>
                </div>
 
+               
                <div class="row">
-                    <div class="col-lg-12 col-md-6 sol-sm-6">
+                    <div class="col-lg-12">
                          <!-- Marketing / donate & sponsor -->
                          <div class="container marketing">
                               <div class="row text-center donateSec">
                                    <span><strong>Please consider helping us keep live musical theater in the community</strong></span>
                               </div>
-                              <div class="row fade-in-up">
-                                   <div class="col-lg-6 text-center mb-3">
-                                        <img class="rounded-circle" width="140" height="140" src="/images/donateIcon.png">
-                                        <h2>Donate</h2>
-                                        <h5>Consider making a donation now</h5>
-                                        <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/donation-form/d9b4ba19-0df8-4869-9aa8-2b7835686f90">Donate</a></p>
-                                   </div>
-                                   <div class="col-lg-6 text-center mb-3">
-                                        <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
-                                        <h2>Sponsor</h2>
-                                        <h5>Consider becoming a Sponsor for a specific show</h5>
-                                        <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
-                                   </div>
-                              </div>
                          </div>
                     </div>
                </div>
                
+               <div class="row fade-in-up">
+                    <div class="container marketing">
+                         <div class="col-lg-6 text-center mb-3">
+                              <img class="rounded-circle" width="140" height="140" src="/images/donateIcon.png">
+                              <h2>Donate</h2>
+                              <h5>Consider making a donation now</h5>
+                              <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/donation-form/d9b4ba19-0df8-4869-9aa8-2b7835686f90">Donate</a></p>
+                         </div>
+                         <div class="col-lg-6 text-center mb-3">
+                              <img class="rounded-circle" width="140" height="140" src="/images/sponsorIcon.png">
+                              <h2>Sponsor</h2>
+                              <h5>Consider becoming a Sponsor for a specific show</h5>
+                              <p><a class="btn btn-info" href="https://www.zeffy.com/en-US/ticketing/sponsorship-for-the-gondoliers-2">Sponsor</a></p>
+                         </div>
+                    </div>
+               </div>               
           </main>
 
           <!-- Footer -->
