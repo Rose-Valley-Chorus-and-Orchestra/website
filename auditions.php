2a01ae010ccd73d59d2420c2b5e116eadeb597b2
johnnysheps <johnsheppard281@gmail.com>
2025-10-10 17:52:53 -0400

    auditions 3

--- auditions.php
+++ auditions.php
@@ -30,15 +30,32 @@
 
                 <div class="row mt-3">
                     <div class="col-lg-12">
-                        <p><strong>Stage Director: </strong>Chris Fitting</p>
-                        <p><strong>Musical Director: </strong>Florrie Marks</p>
-                        
-                        <p><strong><em>Sign Ups Completed</em></strong></p>
                         <p>
-                            <bstrong>Rehearsal dates and times</strong> tentatively set to start early January 2025 on Sundays 2:00 - 5:00 p.m. and Monday and 
-                            Wednesday  7:30 - 10:00 p.m. at the <u>Roosevelt Community Center</u>, 464 S. Old Middletown Road, Media, PA 19063.
+                            <strong>Stage Director: </strong>Chris Fitting
+                            <strong>Musical Director: </strong>Florrie Marks
+                        </p>                        
+                        <h3>
+                            <strong><em>Sign Ups are completed at this time</em></strong>
+                        </h3>
+                        <p>
+                            <strong>Rehearsal dates and times</strong> tentatively set to start early January 2026 on Sundays 2:00 - 5:00 p.m. and Monday and 
+                            Wednesday  7:30 - 10:00 p.m. at the <span class="text-decoration-underline">Roosevelt Community Center</span>, 464 S. Old Middletown Road, Media, PA 19063.
                         </p>
                     
+                        <p>
+                            <strong>Rehearsal dates and times</strong> tentatively set to start early January 2025 on Sundays 2:00 - 5:00 p.m. and Monday and 
+                            Wednesday  7:30 - 10:00 p.m. at the <span class="text-decoration-underline">Roosevelt Community Center</span>, 464 S. Old Middletown Road, Media, PA 19063.</p>
+                    
+                        <p>
+                            <strong>Tech week dates and times:</strong> 
+                            <ul>Sitzprobe: Saturday April 11, 9:30 a.m.  - 12:30 p.m.</ul>
+                              <ul style="margin-top: -9px;">Tech: Sunday April 12, 1:00 - 10:00 p.m.</ul>
+                  <ul style="margin-top: -9px;margin-bottom: -6px;">Tech run-throughs: Monday, Tuesday, &amp; Wednesday April 13-15, 2026, 6:00 - 10:00 p.m.</ul></p>
+                    <p><b>Show dates and times: <span style="font-weight: normal; font-size:0.8em">(Currently date are tentative)</span></b>
+<ul style="margin-top: -15px;">April 18, 19, 25, &amp; 26 at 2:00 p.m.</ul>
+                         <ul style="margin-top: -9px;margin-bottom: -6px;">April 18, 22, &amp; 25 at 7:30 p.m.</ul></p>
+
+<p><b>Performance Site:</b> <u>Strath Haven Middle School</u>, 200 South Providence Road, Wallingford, PA 19086</p>
                     </div>
                 </div>
             </div>
