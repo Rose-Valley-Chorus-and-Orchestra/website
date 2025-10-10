990c80218fe3833c50b2b001aa0b95b11925b1e2
johnnysheps <johnsheppard281@gmail.com>
2025-10-10 17:30:32 -0400

    container added

--- auditions.php
+++ auditions.php
@@ -6,20 +6,23 @@
 
         <!-- Main content -->
         <main>
-            <div class="row mt-3 slide-in-right">
-                <div class="col-12 text-end">
-                    <h1 class="display-3">Auditions</h1>
+            <div class="container">
+                <div class="row mt-3 slide-in-right">
+                    <div class="col-12 text-end">
+                        <h1 class="display-3">Auditions</h1>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row mt-3">
-                <div class="col-lg-12">
-                    <h4 class="text-center">
-                        <strong>Note</strong>:  RVCO is a non-union, non-paying community theater organization. We encourage performers of all races and ethnicities, 
-                        gender identities, sexualities, and abilities to audition.
-                    </h4>
+                <div class="row mt-3">
+                    <div class="col-lg-12">
+                        <h4 class="text-center">
+                            <strong>Note</strong>:  RVCO is a non-union, non-paying community theater organization. We encourage performers of all races and ethnicities, 
+                            gender identities, sexualities, and abilities to audition.
+                        </h4>
+                    </div>
                 </div>
             </div>
+            
         </main>
 
         <!-- Footer -->
