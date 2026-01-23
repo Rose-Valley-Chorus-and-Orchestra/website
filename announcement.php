d015e74c5e4cd46ac39713063f5beeb7c7b8479e
wbmichael99 <wbmichael99@gmail.com>
2026-01-23 11:04:22 -0500

    Adding banner for H2$ delay
    Created announcement.php for the delay of H2$ rehearsals. This banner was added to the home page and the audition page.
    
    Also, updated the copyright in the footer to 2026.

---
+++ announcement.php
@@ -0,0 +1,5 @@
+<div id="announcement" class="responsive">
+	<div style="color: #A30029;font-size: 1.2em;">Delayed rehearsal start date&nbsp;for<br /> 
+          <i>How to Succeed in Business Without Really&nbsp;Trying</i></div>
+     <div style="text-align: left; padding-top: 10px;"><u>WEATHER UPDATE</u>: Due to the impending weather, <!--The Roosevelt Community Center is closing on Sunday January 25 and Monday January 26. Therefore, -->rehearsals will begin on Wednesday January&nbsp;28 at 7:30&nbsp;pm&nbsp;-&nbsp;10&nbsp;pm.</div>
+</div>
