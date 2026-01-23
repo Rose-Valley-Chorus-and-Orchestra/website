23e2fb17bb1310e2560b6d12449826163f5c27af
wbmichael99 <wbmichael99@gmail.com>
2026-01-23 13:28:01 -0500

    more minor changes
    made the text responsive, speed up the movement of the scroll, and another change

--- announcement.php
+++ announcement.php
@@ -7,9 +7,9 @@
 
 
 <div  id="announcement" class="vwrap"><div class="vmove">
-  <div class="vitem" style="color: #A30029;font-size: 1.2em;"><b>Delayed rehearsal start date&nbsp;for</b></div> 
-  <div class="vitem" style="color: #A30029;font-size: 1.2em;"><i>How to Succeed in Business Without Really&nbsp;Trying</i></div>
-  <div class="vitem">&nbsp;</div>
-  <div class="vitem">Due to the impending weather, rehearsals will begin on </div>
-  <div class="vitem"><b>Wednesday January&nbsp;28</b>&nbsp;at 7:30&nbsp;pm&nbsp;-&nbsp;10&nbsp;pm.</div>     
+  <div class="vitem responisive" style="color: #A30029;font-size: 1.2em;"><b>Delayed rehearsal start date&nbsp;for</b></div> 
+  <div class="vitem responisive" style="color: #A30029;font-size: 1.2em;"><i>How to Succeed in Business Without Really&nbsp;Trying</i></div>
+  <div class="vitem responisive">&nbsp;</div>
+  <div class="vitem responisive">Due to the impending weather, rehearsals will begin on </div>
+  <div class="vitem responisive"><b>Wednesday January&nbsp;28</b>&nbsp;at 7:30&nbsp;pm&nbsp;-&nbsp;10&nbsp;pm.</div>     
 </div></div>
