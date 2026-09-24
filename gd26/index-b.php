3413142fc05809756beaa2c7e9acde8e7c77fd28
wbmichael99 <wbmichael99@gmail.com>
2026-09-24 09:09:44 -0400

    Update index-b.php

--- gd26/index-b.php
+++ gd26/index-b.php
@@ -200,136 +200,60 @@ function MM_showHideLayers() { //v9.0
                     <li><a class="dropdown-item" href="../howTo2026/dance/Brotherhood.php" title="Go to the Brotherhood of Man choreography page" style="margin-left: -10px;">Brotherhood of Man</a></li>
                     </ul> -->
           </div>
-</div>
+     </div>
                
-               <div id="castPage" style="clear: both;">
-                    <h1 id="pageName"><i>The Grand Duke</i> Cast Page</h1> 
+     <div id="castPage" style="clear: both;">
+          <h1 id="pageName"><i>The Grand Duke</i> Cast Page</h1> 
 
                     
-               <ul class="nav nav-tabs">
-                 <li id="tab0" style="display: inline;" class="active"><a data-toggle="tab" href="#home">Schedule</a></li>
-                 <li id="tab1"><a data-toggle="tab" href="#menu1" onclick="
-                                                       MM_changeProp('tab0','','display','none','LI');
-                                                       MM_changeProp('tab1','','class','active','LI');
-                                                       MM_changeProp('tab2','','class','','LI');
-                                                       MM_changeProp('tab3','','class','','LI');
-                                          MM_changeProp('home','','display','none','DIV');
-                                          MM_changeProp('menu1','','dsiplay','inline','DIV');
-                                          MM_changeProp('menu2','','display','none','DIV');
-                                          MM_changeProp('menu3','','display','none','DIV');
-                                          ">Videos</a></li>
-                 <li id="tab2"><a data-toggle="tab" href="#menu2">Cast List</a></li>
-                 <li id="tab3"><a data-toggle="tab" href="#menu3">Other</a></li>
-               </ul>
+          <ul class="nav nav-tabs">
+            <li id="tab0" style="display: inline;" class="active"><a data-toggle="tab" href="#home">Schedule</a></li>
+            <li id="tab1"><a ddddata-toggle="tab" href="#menu1" onclick="
+                                                  MM_changeProp('tab0','','class','none','LI');
+                                                  MM_changeProp('tab1','','class','active','LI');
+                                                  MM_changeProp('tab2','','class','','LI');
+                                                  MM_changeProp('tab3','','class','','LI');
+                                     MM_changeProp('home','','display','none','DIV');
+                                     MM_changeProp('menu1','','dsiplay','inline','DIV');
+                                     MM_changeProp('menu2','','display','none','DIV');
+                                     MM_changeProp('menu3','','display','none','DIV');
+                                     ">Videos</a></li>
+            <li id="tab2"><a ddddata-toggle="tab" href="#menu2" onclick="
+                                                  MM_changeProp('tab0','','display','none','LI');
+                                                  MM_changeProp('tab1','','class','','LI');
+                                                  MM_changeProp('tab2','','class','active','LI');
+                                                  MM_changeProp('tab3','','class','','LI');
+                                     MM_changeProp('home','','display','none','DIV');
+                                     MM_changeProp('menu1','','dsiplay','none','DIV');
+                                     MM_changeProp('menu2','','display','inline','DIV');
+                                     MM_changeProp('menu3','','display','none','DIV');
+                                     ">Cast List</a></li>
+            <li id="tab3"><a data-toggle="tab" href="#menu3">Other</a></li>
+          </ul>
 
-               <div class="tab-content">
-                      <div id="home" class="tab-pane fade in active" style="display: inline;">
-                             <?php include("GDschedule.php"); ?>
-                      </div>
-                      <div id="menu1" class="tab-pane fade" style="display: none;">
-                             <h3>Menu 1</h3>
-                             <div><?php include("video.php"); ?></div>
-                      </div>
-                      <div id="menu2" class="tab-pane fade" style="display: none;">
-                             <h3>Cast List</h3>
-                             <div><?php include("TGD_Ensemble.php"); ?></div>
-                      </div>
-                      <div id="menu3" class="tab-pane fade" style="display: none;">
-                             <h3>Other</h3>
-                             <p>Some content in menu 2.</p>
-                      </div>
-               </div>
+          <div class="tab-content">
+                 <div id="home" class="tab-pane fade in active" style="display: inline;">
+                        <?php include("GDschedule.php"); ?>
+                 </div>
+                 <div id="menu1" class="tab-pane fade" style="display: none;">
+                        <h3>Menu 1</h3>
+                        <div><?php include("video.php"); ?></div>
+                 </div>
+                 <div id="menu2" class="tab-pane fade" style="display: none;">
+                        <h3>Cast List</h3>
+                        <div><?php include("TGD_Ensemble.php"); ?></div>
+                 </div>
+                 <div id="menu3" class="tab-pane fade" style="display: none;">
+                        <h3>Other</h3>
+                        <p>Some content in menu 2.</p>
+                 </div>
+          </div>
                     
      
                     
                     
-     <p style="clear: both; padding-top: 15px;">If you have not already paid your dues, new and current members can <a href="https://www.zeffy.com/en-US/ticketing/d1bc3a65-da3a-418e-a989-90ea3f973062" title="Go to Zeffy to pay your dues" >go to Zeffy</a> to pay them.   If you have any questions, please reach out to Lisa Franks: <a href="mailto:members@rvco.org>subject=RVCO Membership" title="Send an email to the membership chair">members@rvco.org</a>.</p>
-<h2>Vocal Score</h2>
-<div class="Rtimes" style="clear: both; padding-top: 0px;">
-     <ol>
-          <li>Print the entire score (double-sided); it's 349 pages, so 175 sheets of paper.</li>
-          <li>If you would like to omit the editor's notes and the Overture, print the following pages, double-sided:
-               pages 3-6, 27-349<br>
-               [Pages 3-6 are the title page and the contents.]</li>
-          <li>Use on a tablet.</li>
-     </ol>
-     <a href="../grandDuke2026/Grand Duke Vocal Score RVCO 2026 - 2 final.pdf" title="Download The Grand Duke libretto" target="new"><span class="button" href="">Download</span></a></div>
-<div style="margin:20px 0 0 30px; padding: 0;"><b>Bios</b>:
-     <ul>
-          Send to Bill Michael and Chris Rubino: <a href="mailto:wbmichael@comcast.net,cmrubino@gmail.com?subject=Here's my bio" title="Email my bio to Bill">wbmichael@comcast.net and cmrubino@gmail.com</a> by September 14<sup>th</sup>
-     </ul>
-</div>
-<!-- <div class="notification">The <b>costume fitting</b> will take place Saturday, March 16 at our Firehouse located at <a href="https://www.google.com/maps/place/184+Bortondale+Rd,+Media,+PA+19063/@39.9017988,-75.3992073,17z/data=!3m1!4b1!4m6!3m5!1s0x89c6e8fb077ef113:0xc1aaad1af713cf34!8m2!3d39.9017988!4d-75.3966324!16s%2Fg%2F11c3q3ffv8?entry=ttu" title="Go to Google Maps" target="new" titler="Go to Google Maps">184 Bortondale Road, Media</a>
-                                   <ul class="notificationTime"><li><b>Men</b>: 10:00 am - 11:00 am</li>
-                                         <li><b>Women</b>: 11:00 am - 12:00 pm</li></ul>
-                                         <div>Please let <a href="mailto:lauraryandesigns@gmail.com?subject=March 16 costume fitting" title="Send an email to our costumer Laura" >Laura</a> know if are not able to be there or can't make your allotted time.</div></div>
+     </div>
 
-                    <div style="margin:0 0 0 30px; padding: 0;"><b>Costumes</b>: 
-                         <ul><li>Fitting Signup: <a href="https://www.signupgenius.com/go/4090D4DAFAA23A0FB6-62431339-rvcos" title="Go to SignUpGenius to select a time" target="new">Costume SignUpGenius</a></li>
-                         <li>Questionnaire: <a href="https://docs.google.com/forms/d/e/1FAIpQLSdGzfcNnsL16Ko75I8hqD7QxGPO5L-4fs3q2vcDTwpEynM3Ew/viewform?usp=dialog" title="Fill out this costume questionaire" target="new">Go to Google Form</a></li>
-                         </ul>
-                    </div>
-                    <div style="margin:0 0 0 30px; padding: 0;"><b>State Stret Pub</b>: <!--<img src="../images/new.png">
-                         <ul><li>Please join your fellow cast members following Sunday rehearsals in March at the State Street Pub in Media.</li>
-                         <li>37 E. State Street, Media <a href="https://www.statestreetpubmedia.com" title="Go to the State Steet Pub website" target="new">https://www.statestreetpubmedia.com</a></li>
-                         </ul>
-                    </div>
-                    <div style="margin:0 0 0 30px; padding: 0;"><b>Order a Show T-shirt</b>: <img src="../images/new.png"> <a href="https://shepscustomdesigns.com/product-category/rvco/" title="Go to the How to Succeed t-shirt order form" target="new">https://shepscustomdesigns.com/product-category/rvco/</a>
-                         </ul>
-                    </div>
-               </p>
-               <p>Access: <a title="Jump down to the list of cast emails" href="#emails">Cast emails</a>;  <a title="Jump down to listen or downlaod the dance music" href="#music">Dance music</a>; <a title="See setting, actors, song, director, and notes for each scene " href="../cinderella/CinderPageDetails.php">Scene Details</a>; <a title="See the set designs for Cinderella" href="../cinderella/CinderSetDesign.php">Set Design</a></p>
-
-               <p>Print out the PDF of the <a href="../cinderella/show_fliers/SponsorandAdForm-Cinderella.pdf" title="Open the sponsorhsip form in a new tab to print" target="new">Sponsorship form</a> or <a href="../cinderella/show_fliers/Cinderella show flier.pdf" title="Open the flier in a new tab" target="new">Cinderella show flier</a>.</p>
-               <p>Download the show flier images to post on social media: <a href="../cinderella/show_fliers/Cinderella flier_front.jpg" title="Open the sponsorhsip form in a new tab to download" target="new">Show flier front (jpg)</a>, <a href="../cinderella/show_fliers/Cinderella flier_back.jpg" target="new">Show flier back (jpg)</a></p>
-
-               <div style="margin: 15px 2px 2px; width: 90%;">Go to <a href="#pictures">pictures</a></div> -->
-<div class="feature" style="clear: both;">
-<p class="headlines"></p>
-<h2>View or Download</h2>
-<ul class="downLinks" style="padding-left: 55px;">
-     <li><a href="../grandDuke2026/Grand Duke vocal score RVCO 2026 errata variant list.pdf" target="_blank" rel="noreferrer noopener" title="Open the Microsoft Word version of the vocal score errata/variants">Vocal score errata/variants</a></li>
-     <li><a href="../grandDuke2026/Grand Duke glossary.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of The Grand Duke glossary">The Grand Duke glossary</a></li>
-     <li><a href="../grandDuke2026/GD - Dialogue edit p. 351 & 352.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF of dialogue edits for pages 351 and 352">pp. 351-2 diaolgue edits</a></li>
-     <!--<li><a href="../grandDuke2026/The Grand Duke - RVCO - rehearsal schedule - Final.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of the rehearsal schedule">Rehearsal Schedule (original)</a></li>
-                                             <li><a href="/gondoliers/flyer/Gondoliers show flyer_NP.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of the show flier">Show flyer</a></li>
-                                             <li><a href="/gondoliers/sponsor and ad/SponsorandAdForm-Gondoliers.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of the sponsor and ad form">Sponsor and Ad form</a></li>
-                                             <li><a href="../grandDuke2026/The Grand Duke - RVCO - rehearsal schedule - Final.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of the rehearsal schedule">Rehearsal Schedule (original)</a></li>-->
-     <li><a href="TGD_Ensemble.php" target="_top" rel="noreferrer noopener" title="Open the Cast List page">Cast emails and phone numbers</a></li>
-     <li><a href="../grandDuke2026/artwork/poster/The Grand Duke poster.pdf" target="_blank" rel="noreferrer noopener" title="Open a PDF version of the show poster">Show poster</a></li>
-</ul>
-<!--<p><a href="../gondoliers/pirates platforms.php" target="_top" title="View the set platfrm layuot">Set platform layout</a></p>-->
-<div class="schedule">
-<h2>Schedule</h2>
-<div class="Rtimes"><b>Rehearsal Times:</b>Sundays: 2:00-5:00 p.m.; Mondays &amp; Wednesdays: 7:30-10:00 p.m.
-     <ul>
-          <li>If you know you will be late, leaving early, or not at rehearsal, please email<a href="mailto:florrie@barks.org,juliecmay@msn.com,?subject=Missing rehearsal">Florrie and Julie</a>.</li>
-          <li><u>Day of rehearsal</u>, please text Florrie at (610) 453-1407 (cell) or Julie at (610) 564-3544</li>
-     </ul>
-</div>
-<div class="Rtimes"><a href="../grandDuke2026/The Grand Duke - RVCO - rehearsal schedule - Final.pdf" title="Download the schedule" target="new"><span class="button" href="">Download</span></a> a PDF to print.</div>
-<div class="castpageLinks" style="">
-     <div id="ViewDays" style="display:inline;"><a href="#pastDays" title="View ALL rehearsal dates" onclick="
-                                                       MM_changeProp('HideLink','','display','inline','DIV');
-                                                       MM_changeProp('ViewDays','','display','none','DIV');
-                                                                                " >View All rehearsal dates and information</a></div>
-     <div id="HideLink" style="display:none;"><a href="#currentDate" title="Hide past rehearsal dates" onclick="
-                                                       MM_changeProp('HideLink','','display','none','DIV');
-                                                       MM_changeProp('ViewDays','','display','inline','DIV');
-                                                                       " alt="Hide past rehearsal dates">Hide past rehearsal dates</a>
-          <div><a href="#currentDate" style="">Go to current date</a></div>
-          <!-- XXXXXXXXXXXXXXXXXXXXXXXXXXXX past rehearsal dates below XXXXXXXXXXXXXXXXXXXXXXXXXXXX-->
-          <span id="pastDays">
-
-          </span></div>
-     <!--   id="HideLink"   -->
-</div>
-<!-- class="castpageLinks" -->
-<span id="currentDate"></span>
-<!-- XXXXXXXXXXXXXXXXXXXXXXXXXXXX begins remaining rehearsal dates below XXXXXXXXXXXXXXXXXXXXXXXXXXXX-->
-
-<!--ends remaining rehearsal dates -->
-</div>
 <!--ends class="schedule" -->
 </div>
 <!--ends class="feature" -->
